f83248c0429927a9036d20104b8e0247df0c52a8
adiRozental <adirosental1@gmail.com>
2024-06-17 13:29:12 +0300

    login+

---
+++ src/component/default3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Demo2Contact() {
+  return (
+    <div style={{width: "100%", height: "300px", backgroundColor: "white"}}>
+
+        <h1>Contact Us!</h1>
+        <input type="email" placeholder='email' />
+        <button>Send</button>
+
+
+    </div>
+  )
+}
