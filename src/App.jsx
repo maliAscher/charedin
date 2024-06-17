00f763e0485cf09cdf1165d5e46a4c4aaf039d94
maliAscher <malistar839@gmail.com>
2024-06-17 13:58:45 +0300

    update user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from './component/HomePage'
+import UserProfile from './component/UserProfile'
 function App() {
 
   return (
     <>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <UserProfile/>
     </>
   )
 }
