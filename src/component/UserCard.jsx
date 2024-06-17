d04c5bb7a0875244ad772517548a15fe4035aace
Rivki <rivkiii770@gmail.com>
2024-06-17 14:43:23 +0300

    initial user card

--- src/component/UserCard.jsx
+++ src/component/UserCard.jsx
@@ -1,4 +1,10 @@
 const UserCard = () => {
-  return <div>UserCard</div>;
+  return (
+    <div>
+      <img src='' alt='' />
+      <h4>חני בן ארי</h4>
+      <div></div>
+    </div>
+  );
 };
 export default UserCard;
