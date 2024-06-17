f86f801c11f2bbf5d252e5bf4eaffe6d85a74c9d
maliAscher <malistar839@gmail.com>
2024-06-17 12:29:34 +0300

    update-finishing homePage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,18 +4,6 @@ import profileImage from './component/icon-HomePage.jpg'; // וודא שהתמו
 
 function HomePage() {
   return (
-    // <div className="container">
-    //   <div className="text">
-    //     <h1>Welcome to my Web Development Portfolio!</h1>
-    //     <p>
-    //       I'm Lily Smith, a passionate web developer based in USA. Here, you'll get a glimpse of my journey in the world of web development, where creativity meets functionality.
-    //     </p>
-    //     <a href="/terms">Read my Terms and Conditions</a>
-    //   </div>
-    //   <div className="image">
-    //     <img src={profileImage} alt="Lily Smith" />
-    //   </div>
-    // </div>
     <div className="container">
       <div className="image">
         <img src={profileImage} alt="Lily Smith" />
