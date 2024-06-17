55deb06c4d3125644e5244026da038e9f0175866
shani litoff <shanilitov@gmail.com>
2024-06-17 14:04:25 +0300

    add my file

--- src/component/findPeople.jsx
+++ src/component/findPeople.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default function findPeople() {
 
 
     return (
         <div>
-            
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+            />
         </div>
     )
 }
