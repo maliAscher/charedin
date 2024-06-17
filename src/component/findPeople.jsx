29d40bcbdd959cac4801b8db94d27283b01cd63e
shani litoff <shanilitov@gmail.com>
2024-06-17 13:50:31 +0300

    install mui

---
+++ src/component/findPeople.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function findPeople() {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
